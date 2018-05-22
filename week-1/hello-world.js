5eb3ce38b697a9ea489030124886ca0e27c04ef1
Joshua Arrants <jwarrants@my.bellevue.edu>
2018-05-21 19:09:15 -0500

    adding hello_world.js

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,17 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Professor Krasso
+; Date: 21 May 2018
+; Modified By: Josh Arrants
+; Description: This program demonstrates how 
+; to run a JavaScript application in VS Code, 
+; though the NODE.js debugger console. 
+;===========================================
+*/
+
+//start program
+
 console.log("Hello World, my name is Joshua Arrants");
+
+//end program
