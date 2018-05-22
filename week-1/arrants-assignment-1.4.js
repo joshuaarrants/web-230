dff3ec68a2b91b51b9a27535b7049030bd1d0196
Joshua Arrants <jwarrants@my.bellevue.edu>
2018-05-21 20:09:25 -0500

    adding arrants-assignment-1.4.js

---
+++ week-1/arrants-assignment-1.4.js
@@ -0,0 +1,45 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Professor Krasso
+; Date: 21 May 2018
+; Modified By: Josh Arrants
+; Description: This program declares, assigns, and outputs values.
+;===========================================
+*/
+
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "Joshua";
+var lastName = "Arrants";
+var Age = "27"
+var Street = "1607 Seaton Park"
+var City = "Montgomery"
+var State = "AL"
+var zipCode = "36116"
+
+
+// output 
+console.log(firstName);
+console.log(lastName);
+console.log(Age);
+console.log(Street);
+console.log(City);
+console.log(State);
+console.log(zipCode); 
+
+
+// end program 
