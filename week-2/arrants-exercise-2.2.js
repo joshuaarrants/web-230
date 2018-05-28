8fbbd8cc5a03ac8ad9a83a617059b62a1725ca34
Joshua Arrants <jwarrants@my.bellevue.edu>
2018-05-28 11:39:54 -0500

    adding header.js

---
+++ week-2/arrants-exercise-2.2.js
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title: Assignment 2.2
+; Author: Professor Krasso
+; Date: 28 May 2018
+; Modified By: Joshua Arrants
+; Description: This program will call the header.js file.
+;===========================================
+*/
+
+//start program
+
+// variable declaration and assignment goes here ...
+var header = require('../header.js');
+
+//output
+console.log("\nTesting the header.js file's output..."); 
+console.log(header.display("Joshua", "Arrants", "Exercise 2.2")); 
+console.log("\n");
+
+//end of program
