0bf7b1c1c7f7da95e171509127fcc1b3a9f5edbf
Joshua Arrants <jwarrants@my.bellevue.edu>
2018-06-16 23:31:22 -0500

    adding arrants-assignment-4.4.js

---
+++ week-4/arrants-assignment-4.4.js
@@ -0,0 +1,80 @@
+/*
+============================================
+; Title: Assignment 4.4
+; Author: Professor Krasso
+; Date: 11 June 2018
+; Modified By: Joshua Arrants
+; Description:
+======
+*/
+
+var header = require('../header.js');
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Assignment 4.4
+ Today's Date
+
+ -- ORIGINAL ARRAY --
+ Alabama
+ Nebraska
+ Iowa
+ California
+ Nevada
+
+ -- SORTED ARRAY --
+ Alabama
+ California
+ Iowa
+ Nebraska
+ Nevada
+
+ -- SELECTED VALUE --
+ Iowa
+
+ */
+
+ // output header
+  console.log(header.display("Joshua", "Arrants", "Assignment 4.4"));
+  console.log('\n'); 
+
+// start program
+
+//variable
+var states = ["Alabama","Nebraska","Iowa","California","Nevada"];
+
+
+// function
+function getValue(x,val) {
+    return val%2==0;
+}  
+// Output
+
+
+
+//Output
+console.log("-- ORIGINAL ARRAY --");
+for(var h = 0; h < states.length; h++) {
+    console.log(states[h]);
+}
+
+//new line
+console.log("");
+
+//Output
+console.log(" -- SORTED ARRAY --");
+for(var i = 0; i < states.length; i++) {
+    states.sort();
+    console.log(states[i]);
+}
+
+//new line
+console.log("");
+filterStates = states.filter(getValue);
+console.log("-- SELECTED VALUE --");
+console.log(filterStates[1]);
+
+
+// end program
